123d84e3182d8884d24de9aff87f3f07bc65a1ba
Conte <boxiaozhi.bolu@gmail.com>
2020-02-17 21:51:30 +0800

    change api

--- src/api/base.js
+++ src/api/base.js
@@ -1,4 +1,4 @@
 export default {
     api_isconte: 'http://api.isconte.com',
-    k_isconte: 'http://192.168.0.5:3000',
+    k_isconte: 'http://k.isconte.com',
 }
---
+++ src/views/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        B
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
