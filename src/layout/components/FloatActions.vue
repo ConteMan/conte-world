ab980793c2ddc9044f926268f983f885f174203c
ConteMan <boxiaozhi.bolu@gmail.com>
2020-12-23 06:20:10 +0800

    style: 修改深色模式细节
    - 深色模式下,悬浮操作按钮设置背景色;
    - 电影列表细节修改,分页适应主题红色;
    - 语雀发布文章,展示效果和原生兼容,深色模式兼容;

--- src/layout/components/FloatActions.vue
+++ src/layout/components/FloatActions.vue
@@ -57,23 +57,21 @@ export default {
 
   .item {
     margin: 10px 0 0 0;
-
     .ant-btn-background-ghost {
       color: @font-color-grey;
       border-color: @grey;
     }
-
     .ant-btn:hover, .ant-btn:focus {
       color: @font-color-grey;
       border-color: @grey-60;
       box-shadow: none;
       animation: jelly 0.5s;
     }
-
     .ant-btn {
       padding: 0 10px;
       border: 1px solid @border-color;
       border-radius: 1px;
+      background: @bg !important;
     }
 
     --antd-wave-shadow-color: none;
@@ -97,20 +95,17 @@ export default {
 
 .dark-theme {
   .main-container {
-    background: @dark-theme-bg;
-
     .item {
       .ant-btn-background-ghost {
         color: @dark-theme-color;
         border-color: @dark-theme-color;
       }
-
       .ant-btn:hover, .ant-btn:focus {
         color: @dark-theme-color;
       }
-
       .ant-btn {
         border: 1px solid @dark-theme-color;
+        background: @dark-theme-bg !important;
       }
     }
   }
