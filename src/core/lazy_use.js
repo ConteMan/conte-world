eb39c5f7733f55402343f0232252a79cfa7b51a5
ConteMan <real.conteman@gmail.com>
2021-07-02 22:46:38 +0800

    :sparkles:  feat: 删除谷歌统计等;
    - [删除] 删除谷歌统计;
    - [修改] 更换 favicon;

--- src/core/lazy_use.js
+++ src/core/lazy_use.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import router from '@/router';
 import VueStorage from 'vue-ls';
 
 // components
@@ -9,26 +8,10 @@ import '@/core/lazy_lib/components';
 import setting from '@/config/setting'; // 全局配置
 
 // extend
-import VueGtag from 'vue-gtag';
 import dayjs from 'dayjs';
 import _ from 'lodash';
 
 Vue.use(VueStorage, setting.storageOptions);
-
-if (process.env.NODE_ENV === 'production') {
-  Vue.use(
-    VueGtag,
-    {
-      config: {
-        id: process.env.VUE_APP_BASE_GA,
-        params: {
-          send_page_view: false,
-        },
-      },
-    },
-    router,
-  );
-}
 
 Vue.prototype.$config = setting;
 Vue.prototype.$dayjs = dayjs;
