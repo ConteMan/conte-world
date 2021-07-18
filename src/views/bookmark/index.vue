7a0a304c6825b8d9699521e7cba776831f3bfa06
ConteMan <real.conteman@gmail.com>
2021-07-18 19:26:25 +0800

    :sparkles:  feat: bOOKMARK 模块增加 OPML 转 JSON 功能;

--- src/views/bookmark/index.vue
+++ src/views/bookmark/index.vue
@@ -4,10 +4,14 @@
       <a-icon type="left" @click="$router.push({ path: '/' + type })" />
     </div>
     <div class="content">
-      <div class="fileInput">
-        <button @click="() => $refs['fileInput'].click()">选择书签文件转换为 JSON 格式</button>
+      <div class="file-input-container">
+        <button @click="() => $refs['bookmarkFileInput'].click()">选择书签文件转换为 JSON 格式</button>
+        <input ref="bookmarkFileInput" type="file" style="display: none" @change="bookmarkChange" />
+      </div>
+      <div class="file-input-container">
+        <button @click="() => $refs['opmlFileInput'].click()">选择 OPML 文件转换为 JSON 格式</button>
         <div v-if="file.name" class="file-name">{{ file.name }}</div>
-        <input ref="fileInput" type="file" style="display: none" @change="fileChange" />
+        <input ref="opmlFileInput" type="file" style="display: none" @change="opmlChange" />
       </div>
       <div v-if="Object.keys(jsonData).length > 0" class="json">
         <pre class="json-pre" v-html="JSON.stringify(jsonData, null, 2)"></pre>
@@ -45,11 +49,34 @@ export default {
     ...mapMutations('app', {
       loadingAction: MT.LOADING,
     }),
-    domDeal(domStr) {
+    // 书签转换按钮动作
+    async bookmarkChange(e) {
+      const current = e.target.files[0];
+      this.file = current;
+      const docStr = await this.readFileAsync(current);
+      const docDom = this.domDeal(docStr);
+      this.jsonData = this.bookmarkDomToJson(docDom.body);
+    },
+    // OPML 转换按钮动作
+    async opmlChange(e) {
+      const current = e.target.files[0];
+      this.file = current;
+      const docStr = await this.readFileAsync(current);
+      const docDom = this.domDeal(docStr, 'application/xml');
+      const version = docDom.querySelector('opml').getAttribute('version');
+      const data = this.opmlDomToJson(docDom.querySelector('body'));
+      this.jsonData = {
+        version,
+        data,
+      };
+    },
+    // DOM 解析
+    domDeal(domStr, type = 'text/html') {
       const parser = new DOMParser();
-      const doc = parser.parseFromString(domStr, "text/html");
+      const doc = parser.parseFromString(domStr, type);
       return doc;
     },
+    // 异步读取文件
     readFileAsync(file) {
       return new Promise((resolve, reject) => {
         const fileReader = new FileReader();
@@ -60,15 +87,8 @@ export default {
         fileReader.readAsText(file);
       });
     },
-    async fileChange(e) {
-      const current = e.target.files[0];
-      this.file = current;
-      const doc = await this.readFileAsync(current);
-      const dealDoc = this.domDeal(doc);
-      this.jsonData = this.domToJson(dealDoc.body);
-    },
-    // DOM 转 JSON
-    domToJson(node) {
+    // 书签 DOM 转 JSON
+    bookmarkDomToJson(node) {
       if (!node){
         return null;
       }
@@ -89,7 +109,7 @@ export default {
         return current;
       }
 
-      for(let i = 0; i < node.childNodes.length; i++){
+      for (let i = 0; i < node.childNodes.length; i++){
         const tempNode = node.childNodes[i];
         const childNodeName = tempNode.nodeName.toLowerCase();
         if (['h1', 'h2', 'h3'].includes(childNodeName)) {
@@ -102,10 +122,38 @@ export default {
           }
         }
         if (['dt'].includes(childNodeName)) { // 子项是 dt，父项一定是 dl
-          current.push(this.domToJson(tempNode));
+          current.push(this.bookmarkDomToJson(tempNode));
         }
         if (['dl'].includes(childNodeName)) {
-          current.children = this.domToJson(tempNode);
+          current.children = this.bookmarkDomToJson(tempNode);
+        }
+      }
+      return current;
+    },
+    // OPML 转 JSON
+    opmlDomToJson(node) {
+      if (!node) {
+        return null
+      }
+
+      const attrs = [ 'text', 'title', 'type', 'xmlUrl', 'htmlUrl' ];
+      let current = {};
+      
+      const currentAttrs = node.attributes;
+      for (let i = 0; i < currentAttrs.length; i++) {
+        const tempAttr = currentAttrs[i];
+        if (attrs.includes(tempAttr.name)) {
+          current[tempAttr.name] = tempAttr.value;
+        }
+      }
+
+      if (node.hasChildNodes()) {
+        current.children = [];
+        for (let i = 0; i < node.childNodes.length; i++) {
+          const tempNode = node.childNodes[i];
+          if (tempNode.nodeName === 'outline') {
+            current.children.push(this.opmlDomToJson(tempNode));
+          }
         }
       }
       return current;
