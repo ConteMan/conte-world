002b24a2185179e695e894df867fa602876ae598
ConteMan <real.conteman@gmail.com>
2021-08-30 16:48:34 +0800

    :wrench:  chore: 依赖修改;

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,15 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
-const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
-const IS_PRO = process.env.NODE_ENV === "production";
+const IS_PRO = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  publicPath: "/",
+  publicPath: '/',
   lintOnSave: false,
   productionSourceMap: false,
   runtimeCompiler: true,
   devServer: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 7777,
   },
 
@@ -29,33 +28,33 @@ module.exports = {
 
     config.optimization = {
       splitChunks: {
-        chunks: "all",
+        chunks: 'all',
         minSize: 3000, // （默认值：30000）块的最小大小。
         minChunks: 1, //（默认值：1）在拆分之前共享模块的最小块数
         maxAsyncRequests: 5, //（默认值为5）按需加载时并行请求的最大数量
         maxInitialRequests: 6, // （默认值为3）入口点的最大并行请求数
-        automaticNameDelimiter: "-",
+        automaticNameDelimiter: '-',
         name: true,
         cacheGroups: {
           lodash: {
-            name: "lodash",
+            name: 'lodash',
             test: /[\\/]node_modules[\\/]lodash[\\/]/,
             priority: 20,
           },
           vue: {
-            name: "vue",
+            name: 'vue',
             test: /[\\/]node_modules[\\/]vue[\\/]/,
           },
           vuex: {
-            name: "vuex",
+            name: 'vuex',
             test: /[\\/]node_modules[\\/]vuex[\\/]/,
           },
-          "vue-router": {
-            name: "vue-router",
+          'vue-router': {
+            name: 'vue-router',
             test: /[\\/]node_modules[\\/]vue-router[\\/]/,
           },
-          "ant-design-vue": {
-            name: "ant-design-vue",
+          'ant-design-vue': {
+            name: 'ant-design-vue',
             test: /[\\/]node_modules[\\/]ant-design-vue[\\/]/,
           },
         },
@@ -65,12 +64,12 @@ module.exports = {
     if (IS_PRO) {
       plugins.push(
         new CompressionWebpackPlugin({
-          filename: "[path].gz[query]",
-          algorithm: "gzip",
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
           test: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i,
           threshold: 10240,
           minRatio: 0.8,
-        })
+        }),
       );
     }
 
@@ -81,12 +80,12 @@ module.exports = {
     config.resolve.symlinks(true);
 
     if (process.env.use_analyzer) {
-      config.plugin("webpack-bundle-analyzer").use(BundleAnalyzerPlugin);
+      config.plugin('webpack-bundle-analyzer').use(BundleAnalyzerPlugin);
     }
 
     if (IS_PRO) {
-      config.store.set("devtool", "");
-      config.plugin("html").tap(([options]) => [
+      config.store.set('devtool', '');
+      config.plugin('html').tap(([options]) => [
         Object.assign(options, {
           minify: {
             removeComments: true,
@@ -106,17 +105,17 @@ module.exports = {
           },
           cache: true, // 仅在文件被更改时发出文件
           hash: true, // true则将唯一的webpack编译哈希值附加到所有包含的脚本和CSS文件中,这对于清除缓存很有用
-          scriptLoading: "defer", // 现代浏览器支持非阻塞javascript加载（'defer'）,以提高页面启动性能。
+          scriptLoading: 'defer', // 现代浏览器支持非阻塞javascript加载（'defer'）,以提高页面启动性能。
           inject: true, // true所有javascript资源都将放置在body元素的底部
-          chunksSortMode: "none",
+          chunksSortMode: 'none',
         }),
       ]);
     } else {
-      config.store.set("devtool", "eval-source-map");
-      config.plugin("html").tap(([options]) => [
+      config.store.set('devtool', 'eval-source-map');
+      config.plugin('html').tap(([options]) => [
         Object.assign(options, {
           minify: false,
-          chunksSortMode: "none",
+          chunksSortMode: 'none',
         }),
       ]);
     }
